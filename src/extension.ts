6bfe8b114866b0ce1c324a8475e0b051ffeb82a6
miriamsh <m0527171443@gmail.com>
2022-07-13 13:24:12 +0300

    Connect to API

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,6 @@
 import * as vscode from 'vscode';
+import axios from 'axios';
+
 
 export function activate(context: vscode.ExtensionContext) {
   vscode.window.registerTreeDataProvider('package-subscriptions', new TreeDataProvider());
@@ -7,12 +9,11 @@ export function activate(context: vscode.ExtensionContext) {
 class TreeDataProvider implements vscode.TreeDataProvider<TreeItem> {
   onDidChangeTreeData?: vscode.Event<TreeItem|null|undefined>|undefined;
 
-  data: TreeItem[];
+  data!: TreeItem[];
 
   constructor() {
-    this.data = [new TreeItem('subscription_1',vscode.TreeItemCollapsibleState.None),
-    new TreeItem('subscription_2',vscode.TreeItemCollapsibleState.None),
-    new TreeItem('subscription_3',vscode.TreeItemCollapsibleState.None)];
+    this.getData().then(d=>
+      this.data=d);
    }
 
   getTreeItem(element: TreeItem): vscode.TreeItem|Thenable<vscode.TreeItem> {
@@ -21,11 +22,74 @@ class TreeDataProvider implements vscode.TreeDataProvider<TreeItem> {
 
   getChildren(element?: TreeItem|undefined): vscode.ProviderResult<TreeItem[]> {
     if (element === undefined) {
+      
       return this.data;
     }
     return element.children;
   }
+
+  async getData():Promise<TreeItem[]>{
+    
+    await axios.get("http://localhost:5282/api/Location").then(response=>{
+      console.log(response.status);
+    })
+    .then(result=>{
+      console.log(result);
+        this.data = [new TreeItem('subscription_1', vscode.TreeItemCollapsibleState.None),
+          new TreeItem('subscription_2', vscode.TreeItemCollapsibleState.None),
+          new TreeItem('subscription_3', vscode.TreeItemCollapsibleState.None)];
+      })
+    .catch(error=> console.log('error',error));
+   
+    return this.data;
+
+    }
 }
+
+// var myHeaders = new Header();
+    // myHeaders.append("apikey", "jkeLXhptQvmP0mAWuCA9kR0ucF3macfp");
+    
+    // var requestOptions = {
+    //   method: 'GET',
+    //   redirect: 'follow',
+    //   headers:  {"apikey":"jkeLXhptQvmP0mAWuCA9kR0ucF3macfp"}
+    // };
+     
+    // axios.get("https://api.apilayer.com/exchangerates_data/convert?to=GBP&from=EUR&amount=5", requestOptions)
+    //   .then(response =>{
+    //     response.data();
+    //     console.log(response.status);
+    //   } )
+    //   .then(result =>{
+    //     console.log(result);
+    //     this.data = [new TreeItem('subscription_1', vscode.TreeItemCollapsibleState.None),
+    //       new TreeItem('subscription_2', vscode.TreeItemCollapsibleState.None),
+    //       new TreeItem('subscription_3', vscode.TreeItemCollapsibleState.None)];
+    //   } )
+    //   .catch(error => console.log('error', error));
+    // }
+
+  // async getData(){
+  //   try {
+  //       await axios.get<any>('https://www.boi.org.il/currency.xml').then((x)=>{
+  //         if(x)
+  //         {
+  //           this.data= [new TreeItem('subscription_1',vscode.TreeItemCollapsibleState.None),
+  //           new TreeItem('subscription_2',vscode.TreeItemCollapsibleState.None),
+  //           new TreeItem('subscription_3',vscode.TreeItemCollapsibleState.None)];
+  //         }
+  //       });
+        
+  //    } catch (error) {
+  //     if (axios.isAxiosError(error)) {
+  //       console.log('error message: ', error.message);
+  //      } else {
+  //       console.log('unexpected error: ', error);
+  //      }
+  //   }
+  // }
+  
+// }
 
 class TreeItem extends vscode.TreeItem {
   children: TreeItem[]|undefined;
