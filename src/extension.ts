9fcfa0fe105f0861795a2c6a6de0e0a734780118
miriamsh <m0527171443@gmail.com>
2022-07-17 13:00:44 +0300

    Add Icons To The Grid

--- src/extension.ts
+++ src/extension.ts
@@ -1,105 +1,71 @@
 import * as vscode from 'vscode';
 import axios from 'axios';
+import { AzureAccountExtensionApi } from './azure-account.api';
 
 
-export function activate(context: vscode.ExtensionContext) {
+
+export async function activate(context: vscode.ExtensionContext) {
   vscode.window.registerTreeDataProvider('package-subscriptions', new TreeDataProvider());
+
+  //connect to azure account
+  //const azureAccount = vscode.extensions.getExtension<AzureAccountExtensionApi>('ms-vscode.azure-account')!.exports;
+  //if (!(await azureAccount.waitForLogin())) {
+  // 	await vscode.commands.executeCommand('azure-account.askForLogin');
+  // }
+  // else{
+  //   console.log(azureAccount.subscriptions);
+  // }
+
+  // const credential = new VisualStudioCodeCredential();
+  // var token = await credential.getToken(tokenAudience, { tenantId: tenantID });
+  //let azureAccount = vscode.extensions.getExtension<any>('ms-vscode.azure-account');
+  // let importedApi = azureAccount!.activate;
+  //console.log(importedApi.toString());
 }
 
 class TreeDataProvider implements vscode.TreeDataProvider<TreeItem> {
-  onDidChangeTreeData?: vscode.Event<TreeItem|null|undefined>|undefined;
+  onDidChangeTreeData?: vscode.Event<TreeItem | null | undefined> | undefined;
+
 
   data!: TreeItem[];
 
   constructor() {
-    this.getData().then(d=>
-      this.data=d);
-   }
+    // this.onDidChangeTreeData= new vscode.Event<TreeItem | null | undefined>();
+    // this.getData();
+  }
 
-  getTreeItem(element: TreeItem): vscode.TreeItem|Thenable<vscode.TreeItem> {
+  getTreeItem(element: TreeItem): vscode.TreeItem | Thenable<vscode.TreeItem> {
     return element;
   }
 
-  getChildren(element?: TreeItem|undefined): vscode.ProviderResult<TreeItem[]> {
+  async getChildren(element?: TreeItem | undefined): Promise<TreeItem[]> {
     if (element === undefined) {
-      
-      return this.data;
+      return await this.getData();
     }
-    return element.children;
+    //return Promise.all<TreeItem[]>(element.children);
+    return [];
   }
 
-  async getData():Promise<TreeItem[]>{
-    
-    await axios.get("http://localhost:5282/api/Location").then(response=>{
+ async getData(): Promise<TreeItem[]> {
+     return await  axios.get("http://localhost:5282/api/Location").then(response => {
       console.log(response.status);
-    })
-    .then(result=>{
-      console.log(result);
-        this.data = [new TreeItem('subscription_1', vscode.TreeItemCollapsibleState.None),
-          new TreeItem('subscription_2', vscode.TreeItemCollapsibleState.None),
-          new TreeItem('subscription_3', vscode.TreeItemCollapsibleState.None)];
-      })
-    .catch(error=> console.log('error',error));
-   
-    return this.data;
-
-    }
+      return [new TreeItem('subscription_1', vscode.TreeItemCollapsibleState.None),
+      new TreeItem('subscription_2', vscode.TreeItemCollapsibleState.None),
+      new TreeItem('subscription_3', vscode.TreeItemCollapsibleState.None)];
+    });
+   }
 }
 
-// var myHeaders = new Header();
-    // myHeaders.append("apikey", "jkeLXhptQvmP0mAWuCA9kR0ucF3macfp");
-    
-    // var requestOptions = {
-    //   method: 'GET',
-    //   redirect: 'follow',
-    //   headers:  {"apikey":"jkeLXhptQvmP0mAWuCA9kR0ucF3macfp"}
-    // };
-     
-    // axios.get("https://api.apilayer.com/exchangerates_data/convert?to=GBP&from=EUR&amount=5", requestOptions)
-    //   .then(response =>{
-    //     response.data();
-    //     console.log(response.status);
-    //   } )
-    //   .then(result =>{
-    //     console.log(result);
-    //     this.data = [new TreeItem('subscription_1', vscode.TreeItemCollapsibleState.None),
-    //       new TreeItem('subscription_2', vscode.TreeItemCollapsibleState.None),
-    //       new TreeItem('subscription_3', vscode.TreeItemCollapsibleState.None)];
-    //   } )
-    //   .catch(error => console.log('error', error));
-    // }
-
-  // async getData(){
-  //   try {
-  //       await axios.get<any>('https://www.boi.org.il/currency.xml').then((x)=>{
-  //         if(x)
-  //         {
-  //           this.data= [new TreeItem('subscription_1',vscode.TreeItemCollapsibleState.None),
-  //           new TreeItem('subscription_2',vscode.TreeItemCollapsibleState.None),
-  //           new TreeItem('subscription_3',vscode.TreeItemCollapsibleState.None)];
-  //         }
-  //       });
-        
-  //    } catch (error) {
-  //     if (axios.isAxiosError(error)) {
-  //       console.log('error message: ', error.message);
-  //      } else {
-  //       console.log('unexpected error: ', error);
-  //      }
-  //   }
-  // }
-  
-// }
 
 class TreeItem extends vscode.TreeItem {
-  children: TreeItem[]|undefined;
-  
-  constructor(label: string,  collapsibleState: vscode.TreeItemCollapsibleState, children?: TreeItem[]) {
+  children: TreeItem[] | undefined;
+
+  constructor(label: string, collapsibleState: vscode.TreeItemCollapsibleState, children?: TreeItem[]) {
     super(
-        label,
-         children === undefined ? vscode.TreeItemCollapsibleState.None :
-                                 vscode.TreeItemCollapsibleState.Expanded                        
-      );
+      label,
+      children === undefined ? vscode.TreeItemCollapsibleState.None :
+        vscode.TreeItemCollapsibleState.Expanded
+    );
     this.children = children;
   }
 }
